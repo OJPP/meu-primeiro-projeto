136a0fac23d7b2293254e171e5843e41db740791
ojpp <octavio.j.p.paulo@gmail.com>
2026-03-06 17:23:12 +0000

    :sparkles: Versão inicial do projeto

---
+++ src/main/java/com/ojpp/cr2025002/meu_primeiro_projeto/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.ojpp.cr2025002.meu_primeiro_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
